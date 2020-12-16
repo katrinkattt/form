d426d243f1954cf83ec0dbddf128dc538337c1df
Nkitina Ekaterina <52584077+katrinkattt@users.noreply.github.com>
2020-12-16 17:38:18 +0500

    Update authAction.js

--- reactcode/src/redux/authAction.js
+++ reactcode/src/redux/authAction.js
@@ -2,14 +2,31 @@ import axios from 'axios'
 
 export const fetchUser = name => dispatch => {
   axios.get(`http://localhost:8000/users?name=${name}`)
-    .then((res) => {
-      dispatch({ type: 'UPDATE_USER', payload: { ...res.data[0], isAutorizate: true } })
+    .then(({ data }) => {
+      console.log(data.length)
+      const payload = data.length > 0 ? { ...data[0], isAutorizate: true, userName: name } : { isAutorizate: false }
+      dispatch({ type: 'UPDATE_USER', payload })
+    })
+}
+export const fetchData = (name, pass) => dispatch => {
+  axios.get(`http://localhost:8000/users?name=${name}&pass=${pass}`)
+    .then(({ data }) => {
+      console.log(data.length)
+      const payload = data.length > 0 ? { ...data[0], isAutorizate: true, password: pass, userName: name } : { isAutorizate: false }
+      dispatch({ type: 'GET_DATA', payload })
     })
 }
 
-// export const fetchPass = pass => dispatch => {
-//   axios.get(`http://localhost:8000/users?password=${pass}`)
-//     .then((res) => {
-//       dispatch({ type: 'UPDATE_USER', payload: res.data[0] })
-//     })
-// }
+export const logout = () => ({
+  type: 'LOGOUT'
+})
+
+export const pushData = (name, numer, pass) => dispatch => {
+  axios.get('http://localhost:8000/users')
+    .then(({ data }) => {
+      data.push({ 'name': { name }, 'fone-numer': { numer }, 'pass': { pass } })
+      console.log(data.length)
+      const payload = data.length > 0 ? { ...data[0], isAutorizate: true, password: pass, userName: name } : { isAutorizate: false }
+      dispatch({ type: 'PUSH_DATA', payload })
+    })
+}

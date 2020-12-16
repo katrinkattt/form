a970a67e4ddf43a61f6390a446139e9a2850969f
Nkitina Ekaterina <52584077+katrinkattt@users.noreply.github.com>
2020-12-16 17:36:16 +0500

    Update personal.jsx

--- reactcode/src/components/personal/personal.jsx
+++ reactcode/src/components/personal/personal.jsx
@@ -1,23 +1,21 @@
-import { useState } from 'react'
-import axios from 'axios'
+import React from 'react'
 import Button from '@material-ui/core/Button'
 import { NavLink } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../../redux/authAction'
 
 export default function Personal() {
-  const [userName, setUserName] = useState('')
-  const fetch = () => {
-    axios.get('http://localhost:8000/users')
-      .then((res) => {
-        setUserName(res.data[0].name)
-      })
+  const { userName } = useSelector(state => state.auth)
+  const dispatch = useDispatch()
+  const exit = () => {
+    dispatch(logout())
   }
-  fetch()
 
   return (
     <div className="main_personal">
       <div className="title">Личный кабинет</div>
-      <div className="title">{userName}</div>
-      <NavLink to="/authorization"><Button variant="outlined" color="primary">выйти</Button></NavLink>
+      <div className="title">{ userName }</div>
+      <NavLink to="/authorization"><Button variant="outlined" onClick={() => exit()} color="primary">выйти</Button></NavLink>
     </div>
   )
 }

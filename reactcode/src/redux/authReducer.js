c5e3405104699bd07c0487d8652ea30ec808c815
Nkitina Ekaterina <52584077+katrinkattt@users.noreply.github.com>
2020-12-16 17:38:44 +0500

    Update authReducer.js

--- reactcode/src/redux/authReducer.js
+++ reactcode/src/redux/authReducer.js
@@ -1,8 +1,8 @@
-import axios from 'axios'
+//import axios from 'axios'
 
-export const initialState = {
-  pass: '',
-  name: '',
+const initialState = {
+  password: '',
+  userName: '',
   isAutorizate: false
 }
 
@@ -10,13 +10,13 @@ export const authReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case 'UPDATE_USER':
       return { ...state, ...payload }
-
+    case 'GET_DATA':
+      return { ...payload }
+    case 'PUSH_DATA':
+      return { ...payload }
+    case 'LOGOUT':
+      return { ...state, isAutorizate: false }
     default:
       return state
   }
-}
-
-export const getData = (name, pass) => {
-  axios.get(`http://localhost:8000/users?name=${name}&password=${pass}`)
-    .then((res) => res)
 }

456f6163642af9aa9388dcf8ee5e49edb6dd1018
Nkitina Ekaterina <52584077+katrinkattt@users.noreply.github.com>
2020-12-16 17:36:44 +0500

    Update recoweryPass.jsx

--- reactcode/src/components/recoveryPass/recoweryPass.jsx
+++ reactcode/src/components/recoveryPass/recoweryPass.jsx
@@ -1,24 +1,37 @@
+import React, { useState } from 'react'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
-import '../authorization/authorization.css'
 import { useDispatch } from 'react-redux'
-import { NavLink } from 'react-router-dom'
+import { fetchUser } from '../../redux/authAction'
 
-export default function RecoweryPass(props) {
-  // const { } = useSelector(state=>state.auth)
-  // const state = {
-
-  // }
+export default function RecoweryPass() {
   const dispatch = useDispatch()
+  const [login, setLogin] = useState('')
+  const [code, setCode] = useState('')
+
+  const recowery = name => {
+    if (code !== '') {
+      dispatch(fetchUser(name))
+    }
+  }
+  const addCode = () => {
+    setCode('546427')
+  }
 
   return (
     <div className="main_authorization">
       <div className="title">Востановить пароль</div>
-      <form className="lkd" noValidate autoComplete="off">
-        <div className="input"><TextField value="" onChange={(e) => {  }} label='логин' variant="outlined" /></div>
-        <div className="input"><TextField value="" onChange={(e) => {  }} label='код' variant="outlined" /></div>
+      <form noValidate autoComplete="off">
+        <div className="input"><TextField value={login} onChange={(e) => setLogin(e.target.value)} label="логин" variant="outlined" /></div>
+        <div className="input"><TextField value={code} label="код" variant="outlined" /></div>
+        <div className="btn">
+          <Button variant="outlined" onClick={() => addCode()} color="primary">выслать код </Button>
+        </div>
       </form>
-      <NavLink to="/personal"><Button variant="outlined" color="primary">войти</Button></NavLink>
+      <div className="btn">
+        <Button onClick={() => recowery(login)} variant="outlined" color="primary">войти</Button>
+      </div>
+
     </div>
   )
 }

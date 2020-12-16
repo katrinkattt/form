4b6341c2ad71ac7e95c80852b39d6850cd2a797f
Nkitina Ekaterina <52584077+katrinkattt@users.noreply.github.com>
2020-12-16 17:35:47 +0500

    Update authorization.jsx

--- reactcode/src/components/authorization/authorization.jsx
+++ reactcode/src/components/authorization/authorization.jsx
@@ -1,39 +1,37 @@
 import React, { useState } from 'react'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
-import './authorization.css'
 import { NavLink, withRouter } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { fetchUser } from '../../redux/authAction'
-import { initialState } from '../../redux/authReducer'
+import { useDispatch } from 'react-redux'
+import { fetchData } from '../../redux/authAction'
+import { store } from '../../redux'
 
-const Authorization = (props) => {
+const Authorization = () => {
   const [login, setLogin] = useState('')
   const [pass, setPass] = useState('')
-  useSelector(state => console.log(state))
+
+  store.getState()
 
   const dispatch = useDispatch()
-  const loginHandler = name => {
-    dispatch(fetchUser(name))
+  const loginHandler = (name, pas) => {
+    dispatch(fetchData(name, pas))
   }
-
   return (
     <div className="main_authorization">
       <div className="title">Авторизация</div>
-      <form className="lkd" noValidate autoComplete="off">
+      <form noValidate autoComplete="off">
         <div className="input"><TextField onChange={(e) => setLogin(e.target.value)} value={login} label="логин" variant="outlined" /></div>
         <div className="input"><TextField onChange={(e) => setPass(e.target.value)} value={pass} label="пароль" variant="outlined" /></div>
       </form>
       <Button variant="outlined" onClick={() => loginHandler(login, pass)} color="primary">войти</Button>
-      <div>
+      <div className="btn">
         <div className="text">нет аккаунта? зарегистрируйтесь</div>
         <NavLink to="/registration"><Button variant="outlined" color="primary">зарегистрироваться</Button></NavLink>
       </div>
-      <div>
+      <div className="btn">
         <NavLink to="/recowerypass"><Button variant="outlined" color="primary">забыли пароль</Button></NavLink>
       </div>
     </div>
   )
 }
-
 export default withRouter(Authorization)

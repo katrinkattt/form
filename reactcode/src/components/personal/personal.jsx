9620db66b9eefa4cd3ceeb28f903ff338bd54b5e
Nkitina Ekaterina <52584077+katrinkattt@users.noreply.github.com>
2020-12-18 18:17:12 +0500

    Update personal.jsx

--- reactcode/src/components/personal/personal.jsx
+++ reactcode/src/components/personal/personal.jsx
@@ -1,21 +1,46 @@
-import React from 'react'
-import Button from '@material-ui/core/Button'
+import React, { useState } from 'react'
+import { Button } from '@material-ui/core'
 import { NavLink } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { logout } from '../../redux/authAction'
+import { logout, uploadImg } from '../../redux/authAction'
 
 export default function Personal() {
-  const { userName } = useSelector(state => state.auth)
+  const { id, userName, number, avatar } = useSelector(state => state.auth)
   const dispatch = useDispatch()
-  const exit = () => {
-    dispatch(logout())
+  const [file, setFile] = useState('')
+  const [img, setImg] = useState(avatar)
+  const imgStyle = {
+    backgroundImage: `url(${img})`,
+    backgroundSize: 'cover'
+  }
+  const uploadFile = (e) => {
+    setFile(e.target.files[0])
+    const reader = new FileReader()
+    reader.onloadend = function () {
+      setImg(reader.result)
+    }
+
+    if (file) {
+      reader.readAsDataURL(file)
+    } else {
+      setImg('')
+    }
+    dispatch(uploadImg(img, id))
   }
 
   return (
     <div className="main_personal">
       <div className="title">Личный кабинет</div>
-      <div className="title">{ userName }</div>
-      <NavLink to="/authorization"><Button variant="outlined" onClick={() => exit()} color="primary">выйти</Button></NavLink>
+      <div className="title name">{ userName }</div>
+      <div className="numberProfile">{ number }</div>
+      <div className="avatar" style={imgStyle} />
+      <div className="center">
+        <label htmlFor="upload-photo">
+          <input hidden onChange={uploadFile} id="upload-photo" name="upload-photo" type="file" />
+          <Button color="secondary" variant="contained" component="span">сменить аватар</Button>
+        </label>
+      </div>
+      <NavLink to="/authorization"><Button variant="outlined" onClick={() => dispatch(logout())} color="primary">выйти</Button></NavLink>
     </div>
   )
 }
